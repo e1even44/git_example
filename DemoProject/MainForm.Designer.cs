4c7b8749eeb6a00c7f7b4a279b7b9ae8e1d7d43b
Hilal Karsli <Hilal.Karsli@liebherr.com>
2022-09-23 10:03:17 +0200

    added button changing colors on each click

--- DemoProject/MainForm.Designer.cs
+++ DemoProject/MainForm.Designer.cs
@@ -31,6 +31,7 @@ namespace DemoProject
         {
             this.btnClickThis = new System.Windows.Forms.Button();
             this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnClickThis
@@ -54,11 +55,22 @@ namespace DemoProject
             this.lblHelloWorld.Size = new System.Drawing.Size(0, 16);
             this.lblHelloWorld.TabIndex = 1;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(302, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Click this";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(433, 178);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lblHelloWorld);
             this.Controls.Add(this.btnClickThis);
             this.Margin = new System.Windows.Forms.Padding(4);
@@ -73,6 +85,7 @@ namespace DemoProject
 
         private System.Windows.Forms.Button btnClickThis;
         private System.Windows.Forms.Label lblHelloWorld;
+        private System.Windows.Forms.Button button1;
     }
 }
 
